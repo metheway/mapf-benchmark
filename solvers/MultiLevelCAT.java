8d9f4df9c61b40ece6f87207fe4c69e2be13f67d
gray-m <maxwellhgray@utexas.edu>
2016-12-02 11:04:06 -0600

    added possibly useful CAT interface, updated MultiLevelCAT; todo: integrate MultiLevel into all constrained solvers

---
+++ solvers/MultiLevelCAT.java
@@ -0,0 +1,92 @@
+package solvers;
+
+
+import solvers.astar.State;
+import solvers.states.SingleAgentState;
+import utilities.Conflict;
+import utilities.Coordinate;
+import utilities.Node;
+import utilities.Path;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class implementing a conflict avoidance table for multi-layered solvers.
+ * Wraps a list of conflict avoidance tables, and all methods that shadow CAT methods
+ * call the corresponding method in the last conflict avoidance table (the one relevant
+ * to the most recently added CAT.
+ */
+public class MultiLevelCAT {
+
+    private List<ConflictAvoidanceTable> catList;
+
+    public MultiLevelCAT() {
+        catList = new ArrayList<>();
+    }
+
+    public void addLevel() {
+        catList.add(new ConflictAvoidanceTable());
+    }
+
+    public void removeLevel() {
+        catList.remove(catList.size() - 1);
+    }
+
+    /**
+     * Return the first violation found in any level
+     * @param state state to check
+     * @return the first violation found
+     */
+    public int violation(SingleAgentState state) {
+        for (ConflictAvoidanceTable cat : catList) {
+            int violation = cat.violation(state);
+            if (violation != ConflictAvoidanceTable.NO_CONFLICT) return violation;
+        }
+        return ConflictAvoidanceTable.NO_CONFLICT;
+    }
+
+    public void addPath(Path path) {
+        catList.get(catList.size() - 1).addPath(path);
+    }
+
+    public Conflict simulatePath(Path path, int group) {
+        return catList.get(catList.size() - 1).simulatePath(path, group);
+    }
+
+    public boolean isValid(State state) {
+        return catList.get(catList.size() - 1).isValid(state);
+    }
+
+    public Map<Node, int[]> getAgentDestinations() {
+        return catList.get(catList.size() - 1).getAgentDestinations();
+    }
+
+    public Map<Coordinate, List<Integer>> getGroupOccupantTable() {
+        return catList.get(catList.size() - 1).getGroupOccupantTable();
+    }
+
+    public Map<Coordinate, List<Coordinate>> getCoordinateTable() {
+        return catList.get(catList.size() - 1).getCoordinateTable();
+    }
+
+    public void setRelevantGroups(List<Integer> relevantGroups) {
+        catList.get(catList.size() - 1).setRelevantGroups(relevantGroups);
+    }
+
+    public void setAgentGroups(Map<Integer, Integer> agentGroups) {
+        catList.get(catList.size() - 1).setAgentGroups(agentGroups);
+    }
+
+    public Map<Integer, Integer> getAgentGroups() {
+        return catList.get(catList.size() - 1).getAgentGroups();
+    }
+
+    public List<Integer> getRelevantGroups() {
+        return catList.get(catList.size() - 1).getRelevantGroups();
+    }
+
+
+}
